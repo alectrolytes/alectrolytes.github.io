42fcff83bb7e018aad4559054c917e15305d1ef3
alectrolytes <alectrolytes@gmail.com>
2016-02-01 14:36:52 -0500

    Only reports on flagged meds

--- js/pgx-cds.js
+++ js/pgx-cds.js
@@ -24,8 +24,10 @@ var main = function() {
   });
 
   $('.med').click(function() {
-    var report = $(this).closest('.medication').find('.report');
-    $(this).children('p').clone().appendTo(report);
+    if ($(this).children().hasClass('pgx-warning')) {
+      var report = $(this).closest('.medication').find('.report');
+      $(this).children('p').clone().appendTo(report);
+    }
   });
 };
 
