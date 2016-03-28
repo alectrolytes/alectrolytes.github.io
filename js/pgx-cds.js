3c60c4f600ac1eca74b86472c8f949fe62810f50
alectrolytes <alectrolytes@gmail.com>
2016-03-27 23:29:16 -0400

    not ()

--- js/pgx-cds.js
+++ js/pgx-cds.js
@@ -75,9 +75,9 @@ var main = function() {
     $('#frequency-table tbody').remove();
 
     if ($(this).is('#acetaminophen')) {
-      populateTable($('#strength-table'), acetaminophen('0'));
-      populateTable($('#route-table'), acetaminophen('1'));
-      populateTable($('#frequency-table'), acetaminophen('2'));
+      populateTable($('#strength-table'), acetaminophen['0']);
+      populateTable($('#route-table'), acetaminophen['1']);
+      populateTable($('#frequency-table'), acetaminophen['2']);
     }
   });
 
