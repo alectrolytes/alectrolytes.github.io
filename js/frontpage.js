f3020044911d09431a9a7da6da64771fa6e086a9
Alec <alec@teamnex.us>
2016-09-06 11:46:44 -0400

    added smooth scrolling

--- js/frontpage.js
+++ js/frontpage.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('.project').hover(function() {
     $('.content').fadeIn(500);
   });
+  var $root = $('html, body');
+  $('a').click(function() {
+    $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+  $('img').on('dragstart', function(){return false;});
 });
