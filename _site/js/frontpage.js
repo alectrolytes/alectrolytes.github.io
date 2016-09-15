86516ad57f2af15d209acf9eabb506b101eebb07
Alec <alec@teamnex.us>
2016-09-15 17:12:27 -0400

    Changed profile picture

--- _site/js/frontpage.js
+++ _site/js/frontpage.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
   $('.project').hover(function() {
     $('.content').fadeIn(500);
   });
+
+  //smooth scrolling
+  var $root = $('html, body');
+  $('a').click(function() {
+    $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
 });
